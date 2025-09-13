f78f115ca73d27e53efbc7c26c364d70fc3ede28
Arianna Bikombe <ariannabikombe@gmail.com>
2025-09-13 11:28:57 -0400

    Edit

--- api/mapper/bunny-fact.mapper.ts
+++ api/mapper/bunny-fact.mapper.ts
@@ -1,6 +1,4 @@
-// adjust this type import if your model is BunnyFact instead of bunny_facts
-import type { bunny_facts } from '../generated/prisma'
-
+import type { bunny_facts } from '@prisma/client';
 export type BunnyFactDTO = {
     id: number
     fact: string
@@ -9,4 +7,4 @@ export type BunnyFactDTO = {
 export const toDTO = (row: bunny_facts): BunnyFactDTO => ({
     id: row.id,
     fact: row.fact,
-})
+});
