fc74171075345e08806ff78fba4840dfe0789d5b
Arianna Bikombe <ariannabikombe@gmail.com>
2025-09-14 15:36:05 -0400

    home loading

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
     selector: 'home',
     standalone: true,
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, LoaderComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
@@ -14,4 +15,15 @@ export class HomeComponent {
     header: string = "Hi, I'm";
     name: string = "Arianna";
     mainTitle: string = "Featured Sections";
+    loading: boolean = false;
+
+    constructor() {
+        this.loading = true;
+    }
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.loading = false;
+        }, 2000); // Simulate a 2-second loading time
+    }
 }
